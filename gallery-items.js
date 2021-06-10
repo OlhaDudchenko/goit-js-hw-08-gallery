c294d39ea63a2d01a5e7673c8d0d6a45e7af91e5
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-06-10 09:44:04 +0300

    add task

--- gallery-items.js
+++ gallery-items.js
@@ -1,4 +1,5 @@
-export default [
+export default
+ [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -64,18 +65,3 @@ export default [
   },
 ];
 
-
-
-{/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li> */}
